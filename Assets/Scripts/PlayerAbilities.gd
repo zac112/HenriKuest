23d246921b82a5c3a643cd8620c62c589ab3a574
Timo Rintamäki <timorintamaki90@gmail.com>
2023-05-20 08:50:33 +0300

    Player Abilities changes

--- Assets/Scripts/PlayerAbilities.gd
+++ Assets/Scripts/PlayerAbilities.gd
@@ -2,8 +2,7 @@ extends Node
 
 
 # Declare member variables here. Examples:
-export var maxDistanceToUseTent = 50
-export var maxDistanceToCallSoldiers = 50
+var nearbySoldiers = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,31 +11,27 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	_checkInput()
+	connect("body_entered", self, "_on_body_entered")
+	connect("body_exited", self, "_on_body_exited")
 	
-	
-	
-# Returns a list of soldiers within maxDistanceToCallSoldiers.
-func _getNearbySoldiers():
-	var soldiers = get_tree().get_nodes_in_group("Soldiers")
-	var nearbySoldiers
-	
-	for soldier in soldiers:
-		if soldier.global.position.distance_to(self.global.position) < maxDistanceToCallSoldiers:
-			nearbySoldiers.add(soldier)
-	
-	return nearbySoldiers
 
+func _on_body_entered(body:Node):
+	if body.is_in_group("Soldiers"):
+		nearbySoldiers.add(body.get_parent())
+	
+
+func _on_body_exited(body:Node):
+	if body.is_in_group("Soldiers"):
+		nearbySoldiers.remove(body.get_parent())
+	
 
 func _checkInput():
 	if Input.is_action_pressed("command_troops"):
 		commandSoldiers()
 
-func body_entered(body:Node):
-	print(body)
 	
 
 func commandSoldiers():
-	var nearbySoldiers = _getNearbySoldiers()
 	
 	for soldier in nearbySoldiers:
 		soldier.get_parent.remove_child(soldier)
