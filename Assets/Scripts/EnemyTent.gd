feeb47cee9a1f973ffba9f6137a276b0ae448c9d
Timo Rintamäki <timorintamaki90@gmail.com>
2023-05-20 15:55:47 +0300

    EnemyTent update

--- Assets/Scripts/EnemyTent.gd
+++ Assets/Scripts/EnemyTent.gd
@@ -12,7 +12,7 @@ var parentSquare
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	parentSquare = get_parent().get_parent()
-	var defenders = get_parent().getSoldiers()
+	defenders = get_parent().getSoldiers()
 	add_child(battleTimer)
 	battleTimer.wait_time = 2
 	battleTimer.connect("timeout", self, "_killTroops")
@@ -28,12 +28,11 @@ func _on_body_entered(body:Node):
 
 func _on_body_exited(body:Node):
 	if body.is_in_group("Player") && attackers.size() == 0:
-		combat = false
 		print("Pelaaja poistui vihollisteltan läheltä")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (combat == true):
+	if (combat == true && battleTimer.is_stopped()):
 		battleTimer.start()
 	
 
@@ -42,10 +41,14 @@ func _getAttackersFromPlayer():
 	for soldier in playerSoldiers:
 		attackers.append(soldier)
 		soldier.setTarget(parentSquare)
+		playerSoldiers.clear()
 
 
 func _killTroops():
 	print("Combat in progress!")
+	if attackers == null:
+		return
+	
 	if attackers.size() == 0 || defenders.size() == 0:
 		combat = false
 		battleTimer.stop()
@@ -54,3 +57,6 @@ func _killTroops():
 		attackers.pop_back().queue_free()
 		defenders.pop_back().queue_free()
 		print("Troops killed")
+		
+	print(defenders)
+	print(attackers)
