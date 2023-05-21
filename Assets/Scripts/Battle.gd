eda337f9d2bc859bb58682ea650100167e1db9ee
Timo Rintamäki <timorintamaki90@gmail.com>
2023-05-21 10:52:54 +0300

    Some progress in Battle script

--- Assets/Scripts/Battle.gd
+++ Assets/Scripts/Battle.gd
@@ -17,5 +17,27 @@ func _process(delta):
 	_simulateCombat()
 
 
+# Ends combat if runs out of attackers or defenders, otherwise kills units
 func _simulateCombat():
+	if defenders.size() == 0 || attackers.size() == 0:
+		_endBattle()
+	else:
+		_killUnits()
+
+
+# Kills one attacker and one defender
+func _killUnits():
+	defenders.pop_back().queue_free()
+	attackers.pop_back().queue_free()
+
+
+func _getDefenders():
+	pass
+	
+	
+func _getAttackers():
+	pass
+	
+	
+func _endBattle():
 	pass
