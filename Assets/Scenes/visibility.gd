b945b168f240829682d34aac7f6e78bcadbfc47d
uikanufo <arsiasaarinen@gmail.com>
2023-05-21 18:03:23 +0300

    Main menu works 100% no virus

--- Assets/Scenes/visibility.gd
+++ Assets/Scenes/visibility.gd
@@ -22,15 +22,7 @@ func _on_GameLogo_visibility_changed():
 	get_node(".").visible = true
 
 	
-func _on_Story2_pressed():
-	get_node("Label2").visible = true
-	get_node("Label3").visible = true
-	get_node("Victory").visible = true
-	
-	get_parent().get_node("CanvasLayer/GameLogo").visible = true
-	
-	get_node("StoryLabel").visible = false
-	get_node("Story2").visible = false
+
 
 
 func _on_Storybutton_pressed():
@@ -42,3 +34,17 @@ func _on_Storybutton_pressed():
 	
 	get_node("StoryLabel").visible = true
 	get_node("Story2").visible = true
+	
+	
+func _on_Story2_pressed():
+	
+	
+	
+	get_node("./Label2").visible = true
+	get_node("Label3").visible = true
+	get_node("Victory").visible = true
+	
+	get_parent().get_node("CanvasLayer/GameLogo").visible = true
+	
+	get_node("StoryLabel").visible = false
+	get_node("Story2").visible = false
