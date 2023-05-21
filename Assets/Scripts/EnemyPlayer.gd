43e6af301186c294baed38e3eb5cbe5106270d16
andreiBe <andrei.be05@gmail.com>
2023-05-21 10:59:54 +0300

    Taistelua edelleen

--- Assets/Scripts/EnemyPlayer.gd
+++ Assets/Scripts/EnemyPlayer.gd
@@ -1,9 +1,23 @@
-extends Fighter
+extends Node2D
 
 var team = 1
 var followers = []
+var path
+var timer = Timer.new()
 
 func setTeam(num):
 	self.team = num
 func getFollowers():
 	return followers
+	
+func travelPath(path:Array):
+	add_child(timer)
+	timer.connect("timeout",self,"next")
+	timer.start()
+	self.path = path
+	
+func next():
+	var pos = path.pop_front()
+	if pos : global_position = pos
+	else: timer.stop()
+	add_to_group("AIPlayer")
